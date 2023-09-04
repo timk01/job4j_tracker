211ec440aa0a228a0a0330015d89fc71b9f503f6
Timur <timk01@mail.ru>
2023-09-05 00:07:50 +0300

    4. Переопределение метода [#174890]

---
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -0,0 +1,12 @@
+package ru.job4j.inheritance;
+
+public class JSONReport extends TextReport {
+
+    public String generate(String name, String body) {
+        String separator = System.lineSeparator();
+        return "{" +  separator
+                + "\t\"name\" : \"" + name + "\"" + "," + separator
+                + "\t\"body\" : \"" + body + "\"" + separator
+                + "}";
+    }
+}
