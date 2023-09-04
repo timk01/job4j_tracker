002d5968f511d5798afd95175443b0592ac39493
Timur <timk01@mail.ru>
2023-09-04 23:15:43 +0300

    2. Перегрузка конструктора [#173890]

---
+++ src/main/java/ru/job4j/inheritance/ToyCat.java
@@ -0,0 +1,7 @@
+package ru.job4j.inheritance;
+
+public class ToyCat extends Cat {
+    public boolean canBeWashByWashMachine() {
+        return true;
+    }
+}
--- src/main/java/ru/job4j/oop/Computer.java
+++ src/main/java/ru/job4j/oop/Computer.java
@@ -9,9 +9,20 @@ public class Computer {
     public Computer() {
     }
 
+    public Computer(int ssd, String cpu) {
+        this.ssd = ssd;
+        this.cpu = cpu;
+    }
+
     public Computer(boolean multiMonitor, int ssd, String cpu) {
         this.multiMonitor = multiMonitor;
         this.ssd = ssd;
+        this.cpu = cpu;
+    }
+
+    public Computer(boolean multiMonitor, double ssd, String cpu) {
+        this.multiMonitor = multiMonitor;
+        this.ssd = (int) ssd;
         this.cpu = cpu;
     }
 
@@ -19,11 +30,17 @@ public class Computer {
         System.out.println("Много мониторов: " + multiMonitor);
         System.out.println("SSD: " + ssd + " GB");
         System.out.println("Модель CPU: " + cpu);
+        System.out.println();
     }
 
     public static void main(String[] args) {
-        Computer computer1 = new Computer(true, 500, "Intel Core I7-10700K");
-        computer1.printInfo();
-        Computer computer2 = new Computer();
+        Computer first = new Computer(true, 500, "Intel Core I7-10700K");
+        first.printInfo();
+        Computer second = new Computer();
+        second.printInfo();
+        Computer third = new Computer(256, "AMD Ryzen 5 3600");
+        third.printInfo();
+        Computer forth = new Computer(true, 512.0, "AMD Ryzen 7 3700X");
+        forth.printInfo();
     }
 }
