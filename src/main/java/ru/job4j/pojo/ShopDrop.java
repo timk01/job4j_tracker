8a55a4fa22048d7a96b204e28efe90be38e5f0a9
Timur <timk01@mail.ru>
2023-09-06 15:49:57 +0300

    3.2. Удаление моделей из массива. [#310016]

---
+++ src/main/java/ru/job4j/pojo/ShopDrop.java
@@ -0,0 +1,48 @@
+package ru.job4j.pojo;
+
+public class ShopDrop {
+    public static Product[] delete(Product[] products, int index) {
+        for (int i = index; i < products.length - 1; i++) {
+            products[i] = products[i + 1];
+            if (products[i] != null && i == products.length - 2) {
+                products[i + 1] = null;
+            }
+        }
+        if (products[index] != null && index == products.length - 1) {
+            products[index] = null;
+        }
+        return products;
+    }
+
+    public static void main(String[] args) {
+        Product[] products = new Product[3];
+        products[0] = new Product("Milk", 10);
+        products[1] = new Product("Bread", 4);
+        products[2] = new Product("Egg", 19);
+        for (int i = 0; i < products.length; i++) {
+            Product product = products[i];
+            System.out.println(product.getName());
+        }
+        products[1] = products[2];
+        products[2] = null;
+        for (int i = 0; i < products.length; i++) {
+            Product product = products[i];
+            if (product != null) {
+                System.out.println(product.getName());
+            } else {
+                System.out.println("null");
+            }
+        }
+        Product[] products2 = new Product[5];
+        products2[0] = new Product("Bread", 4);
+        products2[1] = new Product("Egg", 10);
+        products2[2] = null;
+        products2[3] = new Product("Fish", 3);
+        products2[4] = null;
+        Product[] productCopy = new Product[10];
+        System.arraycopy(products, 0, productCopy, 0, 5);
+        for (Product product : productCopy) {
+            System.out.println(product);
+        }
+    }
+}
---
+++ src/test/java/ru/job4j/pojo/ShopDropTest.java
@@ -0,0 +1,106 @@
+package ru.job4j.pojo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ShopDropTest {
+    @Test
+    public void whenDropFirst() {
+        Product[] products = new Product[2];
+        products[0] = new Product("Milk", 10);
+        products[1] = new Product("Bread", 4);
+        Product[] rsl = ShopDrop.delete(products, 0);
+        assertThat(rsl[0].getName()).isEqualTo("Bread");
+        assertThat(rsl[1]).isNull();
+    }
+
+    @Test
+    public void whenDropLast() {
+        Product[] products = new Product[2];
+        products[0] = new Product("Milk", 10);
+        products[1] = new Product("Bread", 4);
+        Product[] rsl = ShopDrop.delete(products, 1);
+        assertThat(rsl[0].getName()).isEqualTo("Milk");
+        assertThat(rsl[1]).isNull();
+    }
+
+    @Test
+    public void whenArrayLengthIs4AndDeleteElementWithIndex1() {
+        Product[] products = new Product[4];
+        products[0] = new Product("Bread", 4);
+        products[1] = new Product("Egg", 10);
+        products[2] = new Product("Milk", 2);
+        products[3] = new Product("Fish", 3);
+        int index = 1;
+        Product[] rsl = ShopDrop.delete(products, index);
+        assertThat(rsl[index].getName()).isEqualTo("Milk");
+        assertThat(rsl[2].getName()).isEqualTo("Fish");
+        assertThat(rsl[products.length - 1]).isNull();
+    }
+
+    @Test
+    public void whenArrayLengthIs5AndDeleteElementWithIndex2() {
+        Product[] products = new Product[5];
+        products[0] = new Product("Bread", 4);
+        products[1] = new Product("Egg", 10);
+        products[2] = new Product("Milk", 2);
+        products[3] = new Product("Fish", 3);
+        products[4] = new Product("Fruit", 8);
+        int index = 2;
+        Product[] rsl = ShopDrop.delete(products, index);
+        assertThat(rsl[1].getName()).isEqualTo("Egg");
+        assertThat(rsl[index].getName()).isEqualTo("Fish");
+        assertThat(rsl[3].getName()).isEqualTo("Fruit");
+        assertThat(rsl[products.length - 1]).isNull();
+    }
+
+    @Test
+    public void whenArrayLengthIs5AndDeleteElementWithIndex2But4ElementIsNull() {
+        Product[] products = new Product[5];
+        products[0] = new Product("Bread", 4);
+        products[1] = new Product("Egg", 10);
+        products[2] = new Product("Milk", 2);
+        products[3] = new Product("Fish", 3);
+        products[4] = null;
+        int index = 2;
+        Product[] rsl = ShopDrop.delete(products, index);
+        assertThat(rsl[1].getName()).isEqualTo("Egg");
+        assertThat(rsl[index].getName()).isEqualTo("Fish");
+        assertThat(rsl[products.length - 2]).isNull();
+        assertThat(rsl[products.length - 1]).isNull();
+    }
+
+    @Test
+    public void whenArrayLengthIs5AndDeleteElementWithIndex2ButItIsNull() {
+        Product[] products = new Product[5];
+        products[0] = new Product("Bread", 4);
+        products[1] = null;
+        products[2] = null;
+        products[3] = null;
+        products[4] = null;
+        int index = 2;
+        Product[] rsl = ShopDrop.delete(products, index);
+        assertThat(rsl[products.length - 4]).isNull();
+        assertThat(rsl[products.length - 3]).isNull();
+        assertThat(rsl[products.length - 2]).isNull();
+        assertThat(rsl[products.length - 1]).isNull();
+    }
+
+    @Test
+    public void whenArrayLengthIs5AndDeleteElementWithIndex0AndArrayIsNotSorted() {
+        Product[] products = new Product[5];
+        products[0] = null;
+        products[1] = null;
+        products[2] = new Product("Bread", 4);
+        products[3] = null;
+        products[4] = new Product("Fish", 3);
+        int index = 0;
+        Product[] rsl = ShopDrop.delete(products, index);
+        assertThat(rsl[products.length - 5]).isNull();
+        assertThat(rsl[products.length - 4].getName()).isEqualTo("Bread");
+        assertThat(rsl[index]).isNull();
+        assertThat(rsl[products.length - 2].getName()).isEqualTo("Fish");
+        assertThat(rsl[products.length - 1]).isNull();
+    }
+}
