26bf33a151801df7993c3f710cd351d860d7de81
Timur <timk01@mail.ru>
2023-09-11 22:49:33 +0300

    3.5. Реализация класса StartUI. Вывод заявки по id [#500748]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -45,6 +45,16 @@ public class StartUI {
                 tracker.delete(id);
                 Item item = tracker.findById(id);
                 System.out.println(item == null ? "Item is deleted successfully." : "Cannot delete the item.");
+            } else if (select == 4) {
+                System.out.println("=== Find item by id ===");
+                System.out.print("Enter id: ");
+                int id = Integer.parseInt(scanner.nextLine());
+                Item item = tracker.findById(id);
+                if (item != null) {
+                    System.out.println(item);
+                } else {
+                    System.out.println("Item with id " + id + " cannot be found");
+                }
             }
             if (select == 6) {
                 run = false;
