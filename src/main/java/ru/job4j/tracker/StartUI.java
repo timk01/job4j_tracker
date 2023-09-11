357b391461943a0bb0bfcdb52d4e2df520c01d73
Timur <timk01@mail.ru>
2023-09-11 23:07:43 +0300

    3.1. Реализация класса StartUI. Добавление заявки [#500744]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -3,6 +3,7 @@ package ru.job4j.tracker;
 import java.util.Scanner;
 
 public class StartUI {
+
     public void init(Scanner scanner, Tracker tracker) {
         boolean run = true;
         while (run) {
