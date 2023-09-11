f36633bcf011cc7b803d6ae19239d754d7a11a66
Timur <timk01@mail.ru>
2023-09-11 21:51:00 +0300

    3.3. Реализация класса StartUI. Изменение заявки [#500746]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -2,9 +2,11 @@ package ru.job4j.tracker;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 public class Item {
-    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss",
+            new Locale("en"));
     private int id;
     private String name;
     private LocalDateTime created = LocalDateTime.now();
