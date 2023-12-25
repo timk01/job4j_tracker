4df2afafe0e99281c6b24bdc1824c1cafc1ee01e
Timur <timk01@mail.ru>
2023-12-25 23:21:40 +0300

    6. Список адресов [#110062]

---
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -0,0 +1,12 @@
+package ru.job4j.stream;
+
+import java.util.List;
+
+public class Profiles {
+
+    public static List<Address> collect(List<Profile> profiles) {
+        return profiles.stream().
+                map(Profile::getAddress).
+                toList();
+    }
+}
